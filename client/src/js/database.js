70ce7662f35ce416dc048691258ba1ebfffd05ce
C K <ctkoo@ucsc.edu>
2022-08-22 22:26:43 -0700

    I forgot I was editing off the 'Develop' folder

---
+++ client/src/js/database.js
@@ -0,0 +1,51 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log("PUT to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ todo: content });
+
+  // Confirmation
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  // Create connection
+  const jateDb = await openDB("jate", 1);
+
+  // Create new text
+  const tx = jateDb.transaction("jate", "readonly");
+
+  // Open up desired object store
+  const store = tx.objectStore("jate");
+
+  // Get all data
+  const request = store.getAll();
+
+  // Confirmation
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
+
+initdb();
