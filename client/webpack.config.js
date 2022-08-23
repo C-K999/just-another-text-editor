69b45b30cc91bfdbecca0000c1db599002d9323b
C K <ctkoo@ucsc.edu>
2022-08-22 23:54:23 -0700

    it launches

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -38,13 +38,6 @@ module.exports = () => {
         theme_color: "#7eb4e2",
         start_url: "./",
         publicPath: "./",
-        icons: [
-          {
-            src: path.resolve("assets/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
-          },
-        ],
       }),
     ],
 
